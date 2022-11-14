58bf40011d1ac22e1fa630f5fb1a661df9073d8c
jyoung0930 <jaronyoung1@gmail.com>
2022-11-14 17:45:19 -0600

    Continued Development
    Added JS for hamburger menu.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const hamburger = document.querySelector(".hamburger");
 const hamburgerMenu = document.querySelector(".hamburger-nav-links");
+const hamburgerMenuLinks = document.querySelectorAll(".hamburger-nav-links li");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("change");
   hamburgerMenu.classList.toggle("slide");
+});
+
+hamburgerMenuLinks.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    console.log(e.target);
+    if ((e.target.className = "ham-link"))
+      hamburgerMenu.classList.toggle("slide");
+  });
 });
