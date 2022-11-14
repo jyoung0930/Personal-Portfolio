079660af33e528476a1980c0ddeb8025ee6144c0
jyoung0930 <jaronyoung1@gmail.com>
2022-11-14 14:59:11 -0600

    Continued Development
    Updated hamburger menu design.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const hamburger = document.querySelector(".hamburger");
-const hamburgerMenu = document.querySelector(".hamburger-menu");
+const hamburgerMenu = document.querySelector(".hamburger-nav-links");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("change");
