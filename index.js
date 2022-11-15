291594cabff631d6b1ba15024195815be0915d99
jyoung0930 <jaronyoung1@gmail.com>
2022-11-15 12:34:38 -0600

    Continued Development
    Added form and hamburger menu closing function.

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const hamburger = document.querySelector(".hamburger");
 const hamburgerMenu = document.querySelector(".hamburger-nav-links");
 const hamburgerMenuLinks = document.querySelectorAll(".hamburger-nav-links li");
-
+// Triggers hamburger menu slide animation
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("change");
   hamburgerMenu.classList.toggle("slide");
 });
-
+// Removes hamburger menu on link click
 hamburgerMenuLinks.forEach((item) => {
   item.addEventListener("click", (e) => {
-    console.log(e.target);
     if ((e.target.className = "ham-link"))
       hamburgerMenu.classList.toggle("slide");
   });
+});
+// Removes hamburger menu on scroll
+window.addEventListener("scroll", (e) => {
+  if (hamburgerMenu.classList.contains("slide")) {
+    hamburgerMenu.classList.remove("slide");
+  }
 });
